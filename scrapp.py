1916ce93e972bb8e48424db06fff70f4efd94390
IQNAJA <warachot.iq@gmail.com>
2025-09-15 13:29:37 +0700

    Refactor anime data fetching functions and update URL handling

--- scrapp.py
+++ scrapp.py
@@ -3,8 +3,8 @@ import requests as req
 import re
 import sys
 
-
-url = "https://myanimelist.net/topanime.php"
+url = "https://myanimelist.net"
+listUrl = url+"/topanime.php"
 
 def fetch_page(url):
     try:
@@ -16,17 +16,57 @@ def fetch_page(url):
         return None
 
 
-def getAnimeList ():
+def getAnimeList (page=1):
     """  ดึงรายชื่ออนิเมะจาก MyAnimeList 
-        * return: list ของ tuple (ลิงก์, ชื่ออนิเมะ)
+        * page: หน้าที่ต้องการ (1,2,3,...)
+        * return: dict {"page": page, "data": [ {"link":..., "name":..., "img":..., "score":...}] }
     """
-    html = fetch_page(url)
-    pattern = r'<a href="([^"]+)"[^>]*class="hoverinfo_trigger"[^>]*>([^<]+)</a>'
-    result = re.findall(pattern, html)
-    return result
+    json = {"page":page,
+            "data":[]}
+    link = listUrl + f"?limit={(page-1)*50}"
+    html = fetch_page(link)
+    pattern = r'<a[^>]*class="hoverinfo_trigger[^"]*"[^>]*href="([^"]+)"[^>]*>.*?<img[^>]*alt="Anime: ([^"]+)"[^>]*data-src="([^"]+)"[^>]*>.*?</a>.*?<span class="text on score-label score-[^"]+">([\d.]+)</span>'
+    result = re.findall(pattern, html, re.DOTALL)
+    for item in result:
+        try:
+            json["data"].append({
+                "link": item[0],
+                "name": item[1].replace('\n', ' ').strip(),
+                "img": item[2].replace("r/50x70","").split("?")[0],
+                "score": float(item[3])
+            })
+        except Exception as e:
+            json["data"].append({
+                "link": item[0],
+                "name": item[1].replace('\n', ' ').strip(),
+                "img": item[2].replace("r/50x70",""),
+                "score": float(item[3]),
+            })            
+    return json
+
+
+
+def getAminePage(animeID):
+    """ ดึงข้อมูลอนิเมะจาก MyAnimeList ตาม ID
+        * animeID: ID ของอนิเมะ (ตัวเลข)
+        * return: json ของหน้ารายละเอียดอนิเมะ หรือ None ถ้าไม่พบ
+    """
+    if not str(animeID).isdigit():
+        return None
+    json = {"id":animeID}    
+    link = url + f"/anime/{animeID}"
+    html = fetch_page(link)
+    namePattern = r'<h1[^>]*class="title-name h1_bold_none"[^>]*>.*?<strong>([^<]+)</strong>'
+    name = re.findall(namePattern, html, re.DOTALL)[0]
+    json["name"] = name.strip()
+    imgPlattern = r'<div[^>]*class="leftside"[^>]*>.*?<img[^>]*src="([^"]+)"'
+    img = re.findall(imgPlattern, html, re.DOTALL)[0]
+    json["leftside"] = {"image":img}
+    
+    return json
 
 
 
 
-print(getAnimeList())
+print(getAminePage(5114))
     
